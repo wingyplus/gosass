6815143bdb514da82cf33127d7bd0677e2400fbf
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-01-05 10:55:26 +0700

    convert Sass_Data_Context to Sass_Context before get output string

--- gosass.go
+++ gosass.go
@@ -3,13 +3,14 @@ package gosass
 // #cgo LDFLAGS: -lsass
 // #include <sass/context.h>
 import "C"
+import "unsafe"
 
 func Render(scss string) ([]byte, error) {
 	ctx := C.sass_make_data_context(C.CString(scss))
 	compiler := C.sass_make_data_compiler(ctx)
 	C.sass_compiler_parse(compiler)
 	C.sass_compiler_execute(compiler)
-	css := C.GoString(C.sass_context_get_output_string(ctx))
+	css := C.GoString(C.sass_context_get_output_string((*C.struct_Sass_Context)(unsafe.Pointer(ctx))))
 	C.sass_delete_compiler(compiler)
 	return []byte(css), nil
 }
