7a96c2de06e66b951b3096d0b0141404e79d824f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-01-05 11:19:00 +0700

    use sass_data_context_get_context instead of convert context manually

--- gosass.go
+++ gosass.go
@@ -3,14 +3,14 @@ package gosass
 // #cgo LDFLAGS: -lsass
 // #include <sass/context.h>
 import "C"
-import "unsafe"
 
 func Render(scss string) ([]byte, error) {
 	ctx := C.sass_make_data_context(C.CString(scss))
 	compiler := C.sass_make_data_compiler(ctx)
 	C.sass_compiler_parse(compiler)
 	C.sass_compiler_execute(compiler)
-	css := C.GoString(C.sass_context_get_output_string((*C.struct_Sass_Context)(unsafe.Pointer(ctx))))
+	ctxOut := C.sass_data_context_get_context(ctx)
+	css := C.GoString(C.sass_context_get_output_string(ctxOut))
 	C.sass_delete_compiler(compiler)
 	C.sass_delete_data_context(ctx)
 	return []byte(css), nil
